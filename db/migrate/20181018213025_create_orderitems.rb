72d98d9b04664407b97f5377c8e55c03803f2ead
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-18 15:37:24 -0700

    Adding controllers for all tables

--- db/migrate/20181018213025_create_orderitems.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrderitems < ActiveRecord::Migration[5.2]
-  def change
-    create_table :orderitems do |t|
-      t.integer :quantity_per_item
-
-      t.timestamps
-    end
-  end
-end
