4a0710e72f5344a9ade1ddd1bd3397ee7c245eaf
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-19 14:29:03 -0700

    changes users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homepage#index'
-  resources :users, only: [:index, :show, :shop]
+  resources :users, only: [:index, :show, :shop, :orders]
 
   resources :orders, :orderitems, :items, :reviews
 
