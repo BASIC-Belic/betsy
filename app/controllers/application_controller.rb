b56d6085cd8f720cdda1ec6b4df4eb92042a95ab
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-19 10:24:24 -0700

    OAuth set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  # before_action :create_guest_user
+  before_action :find_user
+
+  private
+  def find_user
+    if session[:user_id]
+      @login_user = User.find_by(id: session[:user_id])
+    end
+  end
 end
