8550cfed3c70b5102ff217bac9838330050e0379
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-19 09:58:42 -0700

    added method to build user account from github, working on routes and controller logic for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homepage#index'
-  resources :users, :orders, :orderitems, :items, :reviews
-  
+  resources :users, only: [:index, :show, :shop]
+
+  resources :orders, :orderitems, :items, :reviews
+
+  get '/shop', to: 'user#shop', as: shop_path
+
+  get '/history', to: 'user#orders', as: orders_path
 end
