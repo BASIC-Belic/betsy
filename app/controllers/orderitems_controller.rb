0f9aa9474f610fa7cd8f7e52b8cc517ab4cf40f8
Lindsay Terchin <lterchin@gmail.com>
2018-10-20 11:55:16 -0700

    committing changes before pulling new ones from master

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -1,6 +1,37 @@
 class OrderitemsController < ApplicationController
 
 
+  def create
+
+    @order = current_order
+    @order_item = @order.order_items.new(order_item_params)
+    @order.save
+
+  end
+
+
+  def show
+
+  end
+
+
+
+
+
+  def update
+    @order = current_order
+    @order_item = @order.order_items.find(params[:id])
+    @order_item.update_attributes(order_item_params)
+    @order_items = @order.order_items
+  end
+
+
+  def destroy
+    @order = current_order
+    @order_item = @order.order_items.find(params[:id])
+    @order_item.destroy
+    @order_items = @order.order_items
+  end
 
 
   private
