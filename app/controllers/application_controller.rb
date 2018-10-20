dcf4c618e4465d2a194d6f1f8dc14890d8476d49
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-20 16:11:41 -0700

    added user model method is_a_merchant?(given_ids) and changed layout to only show merchant lsits for logged in merchant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   before_action :find_current_user
   before_action :find_all_merchants
+  # before_action :is_a_merchant?
   before_action :current_order
   # before_action :require_login, except: :index
 
@@ -22,6 +23,10 @@ class ApplicationController < ActionController::Base
       merchant.user_id
     end
   end
+
+  # def is_a_merchant?
+  #   @merchant = @merchant_ids.include?(@current_user.id)
+  # end
 
   def require_login
     unless @current_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   # def self.build_from_github(auth_hash)
   #   user = User.new(provider: auth_hash['provider'], uid: auth_hash['uid'], nickname: auth_hash['info']['nickname'], name: auth_hash['info']['name'], email: auth_hash['info']['email'], image_url: auto_hash['info']['image'])
   # end
+
+  def is_a_merchant?(given_ids)
+    return given_ids.include?(self.id)
+  end
 end
