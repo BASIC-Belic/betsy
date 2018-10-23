e9c659b05c44e44d39e86d4d96cb896e9dbb34cc
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-22 18:14:43 -0700

    added method to decrement quantity in orderitem

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,5 +2,8 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   validates :item_id, presence: true
 
+  def decrement_quantity_available(num)
+    self.quantity_available -= num
+  end
 
 end
