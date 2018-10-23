73efa8045a30aa931e4d3d6cb1d3aed936a114fc
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-23 16:16:15 -0700

    Show order confirmation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,9 +28,10 @@ class OrdersController < ApplicationController
 
     if successful
       #clear session data
+    temp_id = session[:order_id]
     session[:order_id] = nil
-      flash[:success] = "Order confirmation number: #{@order.id}. Please save this number to view your order status. Disclaimer: This site is for demonstration purposes only."
-      redirect_to order_path(@order)
+      flash[:success] = "Thank you for your order. Order confirmation number: #{@order.id}. Please save this number to view your order status. Disclaimer: This site is for demonstration purposes only."
+      redirect_to order_item_path(temp_id)
     else
 
       flash.now[:error] =  "Error in submitting order."
