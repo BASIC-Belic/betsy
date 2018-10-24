c871d76dc835cafb2df37f7aae7c60aa3d80fd80
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-24 10:54:03 -0700

    added user to item.yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
   has_many :items
   # has_many :orders
   has_many :reviews
+  
+  validates :nickname, presence: true, uniqueness: {
+    scope: :provider, message: "User: username already exists for this provider."
+  }
+
+  validates :email, presence: true, uniqueness: true
 
   # def self.build_from_github(auth_hash)
   #   user = User.new(provider: auth_hash['provider'], uid: auth_hash['uid'], nickname: auth_hash['info']['nickname'], name: auth_hash['info']['name'], email: auth_hash['info']['email'], image_url: auto_hash['info']['image'])
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,51 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
 
-  it "must be valid" do
-    value(user).must_be :valid?
+  before do
+    @linda = users(:one)
+    @john = users(:two)
+    @june = users(:june)
   end
+
+  describe 'relations' do
+    it "has a list of items" do
+      @linda.must_respond_to :items
+      #NEED TO GIVE A YAML ITEM
+      # @linda.items.each do |item|
+      #   item.must_be_kind_of Item
+      # end
+    end
+
+    it "has a list of reviews" do
+      @linda.must_respond_to :reviews
+      #NEED TO GIVE A YAML REVIEW
+      @linda.must_respond_to :reviews
+      # @linda.reviews.each do |review|
+      #   review.must_be_kind_of Review
+      # end
+    end
+  end
+
+
+
+  describe 'validations' do
+    it 'will create a valid user' do
+      expect(@linda.valid?).must_equal true
+    end
+    it 'will not create a user with '
+  end
+
+
+  describe 'is_amerchant?(given_ids)' do
+  end
+
+
+  #Merchant / Signed in User
+  # Username must be present
+  # Username must be unique
+  # Email Address must be present
+  # Email Address must be unique
+
+
 end
