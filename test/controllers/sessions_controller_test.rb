06fc0f6dedb1867fed06247c077e3efc1d70210d
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-24 14:18:59 -0700

    OAuth testing

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -12,6 +12,33 @@ describe SessionsController do
       get auth_callback_path(:github)
 
       must_redirect_to root_path
+      session[:user_id].must_equal person.id
+      User.count.must_equal start_count
     end
+
+    it "creates an account for a new user and redirects to the root route" do
+      start_count = User.count
+      new_user = User.new(provider: "github", uid: 99999, name: "test_user", nickname: "test_user_nickname", email: "test@user.com")
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(new_user))
+
+      get auth_callback_path(:github)
+
+      must_redirect_to root_path
+      User.count.must_equal start_count + 1
+      session[:user_id].must_equal User.last.id
+    end
+
+    # it 'invalid user data cannot log in' do
+    #   start_count = User.count
+    #
+    #   invalid_new_user = User.new(provider: "github", uid: 1, name: "test_user", nickname: "test_user_nickname", email: "test@user.com")
+    #
+    #   OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(invalid_new_user))
+    #
+    #   get auth_callback_path(:github)
+    #
+    #   User.count.must_equal start_count
+      # flash[:error].must_equal "Unable to log in"
+    # end
   end
 end
