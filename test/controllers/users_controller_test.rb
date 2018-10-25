f3bcf1944cc65fdbcf8a3b8c4f92a784510063bf
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-25 07:18:20 -0700

    usr show tests working

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,4 +4,51 @@ describe UsersController do
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
+
+  before do
+    @linda = users(:one)
+    @john = users(:two)
+    @june = users(:june)
+  end
+
+  describe "show" do
+    it "succeeds for an exisiting user / merchant" do
+
+      get user_path(@linda)
+      must_respond_with :success
+    end
+
+    it "succeeds for an exisiting user / not merchant" do
+
+      get user_path(@june)
+      must_respond_with :success
+    end
+
+    it "renders 404 not_found for a non exiting user" do
+
+      get user_path(0)
+      must_respond_with :not_found
+    end
+  end
+
+  describe "shop" do
+    it "succeeds for an exisiting user / merchant" do
+
+      # get shop_path(@linda)
+      # must_respond_with :success
+    end
+
+    it "succeeds for an exisiting user / not merchant" do
+
+      # get shop_path(@june)
+      # must_respond_with :success
+    end
+
+    it "redirects with flash error message for a non exiting user" do
+
+      # expect(get shop_path(0)).must_redirect_to root_path
+    end
+  end
+
+
 end
