c6f0670b42f61913b3fa1e30b9948c1dbda31682
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-25 18:07:11 -0700

    custom method tests work for orderitem

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -7,17 +7,20 @@ class OrderItem < ApplicationRecord
   def submit_order_item
 
     item = Item.find_by(id: self.item_id)
-
-    item.decrement_quantity_available(self.quantity_per_item)
-    #change order item status
-    self.status = "paid"
-    self.save
+    if item
+      item.decrement_quantity_available(self.quantity_per_item)
+      #change order item status
+      self.status = "paid"
+      self.save
+    end
   end
 
   def find_item_name(num)
     item = Item.find_by(id: num)
-    item_name = item.name
-    return item_name
+    if item
+      item_name = item.name
+      return item_name
+    end
   end
 
   def find_order(num)
--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -17,23 +17,24 @@ describe OrderItem do
       order: nil)
       }
 
+    let(:submit_full_order_item) {
+      @orderitem2.submit_order_item
+      item = @orderitem2.item
+      item.decrement_quantity_available(@orderitem2.quantity_per_item)
+      @orderitem2
+    }
+
   describe 'relations' do
     #REVIEW: REPLACE
-    # it "has a list of items" do
-    #   expect(@linda).must_respond_to :items
-    #   @linda.items.each do |item|
-    #     expect(item).must_be_kind_of Item
-    #   end
-    # end
-    #
-    # it "has a list of reviews" do
-    #   expect(@linda).must_respond_to :reviews
-    #   #NEED TO GIVE A YAML REVIEW
-    #   @linda.must_respond_to :reviews
-    #   @linda.reviews.each do |review|
-    #     expect(review).must_be_kind_of Review
-    #   end
-    # end
+    it "has an item" do
+      expect(@orderitem1).must_respond_to :item
+        expect(@orderitem1.item).must_be_kind_of Item
+    end
+
+    it "has an order" do
+      expect(@orderitem1).must_respond_to :order
+        expect(@orderitem1.order).must_be_kind_of Order
+    end
   end
 
   describe 'validations' do
@@ -64,6 +65,38 @@ describe OrderItem do
 
       expect(invalid_order_item.valid?).must_equal false
       expect(invalid_order_item.errors.messages).must_include :order, :order_id
+    end
+  end
+
+  describe 'submit_order_item' do
+
+    it 'will change the status to paid if order_item is not nil' do
+
+      starting_item_quantity = @orderitem2.item.quantity_available
+      quant_to_be_subtracted = @orderitem2.quantity_per_item
+      expected_final_quant = starting_item_quantity - quant_to_be_subtracted
+      starting_status = @orderitem2.status
+
+    submitted_order = submit_full_order_item
+
+    ending_status = submitted_order.status
+    ending_quant = submitted_order.item.quantity_available
+
+    expect(ending_quant).must_equal expected_final_quant
+    expect(starting_status).must_equal "pending"
+    expect(ending_status).must_equal "paid"
+    end
+
+    it 'will not change status if order_items have no items' do
+
+      empty_order = order_item_missing_item
+      start_stat = empty_order.status
+
+      empty_order.submit_order_item
+
+      end_stat = empty_order.status
+
+      expect(end_stat).must_equal end_stat
     end
   end
 end
