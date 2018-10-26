75f57137abf5b54c97b82b5e66405dda1b11f0f7
Kat Perreira <kat.perreira@gmail.com>
2018-10-26 09:24:43 -0700

    all tests pass

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -2,14 +2,14 @@ require "test_helper"
 
 describe OrderItem do
 
-#   let(:order_item) { OrderItem.new }
-#
-#   it "must be valid" do
-#     value(order_item).must_be :valid?
-#   end
-  before do
+  #   let(:order_item) { OrderItem.new }
+  #
+  #   it "must be valid" do
+  #     value(order_item).must_be :valid?
+  #   end
+  # before do
 
-  end
+  # end
 
   it 'returns the name of an item given the OrderItem id' do
     category = categories(:luggage)
@@ -25,29 +25,30 @@ describe OrderItem do
     item_name = new_orderitem.find_item_name(new_orderitem_item_id)
 
     expect(item_name).must_equal "PotatoHead"
+  end
+    # let(:order_item) { OrderItem.new }
+    #
+    # it "must be valid" do
+    #   value(order_item).must_be :valid?
+    # end
+    before do
+      @item = items(:shoes)
+      @order = orders(:one)
+      @new_order_item = OrderItem.create(order_id: @order.id, item_id: @item.id)
+    end
 
-  # let(:order_item) { OrderItem.new }
-  #
-  # it "must be valid" do
-  #   value(order_item).must_be :valid?
+    it 'returns the name of an item given an item id' do
+      item_name = @item.name
+      find_item_name = @new_order_item.find_item_name(@new_order_item.item_id)
+
+      expect(find_item_name).must_equal item_name
+    end
+
+    it 'returns the order instance linked to an order_item' do
+      find_order = @new_order_item.find_order(@new_order_item.order_id)
+
+      expect(find_order).must_equal @order
+
+    end
   # end
-  before do
-    @item = items(:shoes)
-    @order = orders(:one)
-    @new_order_item = OrderItem.create(order_id: @order.id, item_id: @item.id)
-  end
-
-  it 'returns the name of an item given an item id' do
-    item_name = @item.name
-    find_item_name = @new_order_item.find_item_name(@new_order_item.item_id)
-
-    expect(find_item_name).must_equal item_name
-  end
-
-  it 'returns the order instance linked to an order_item' do
-    find_order = @new_order_item.find_order(@new_order_item.order_id)
-
-    expect(find_order).must_equal @order
-
-  end
 end
