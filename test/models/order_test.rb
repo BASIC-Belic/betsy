fad18b3af40c99ffcf8b4a5e81269fbd9b8d30d0
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-28 10:13:17 -0700

    passing relations test for order model

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,9 +1,31 @@
 require "test_helper"
 
 describe Order do
-  let(:order) { Order.new }
+  before do
+    @pending = orders(:one)
+    @pending.credit_card_exp_month = Date.today.month
+    @pending.credit_card_exp_year = Date.today.year
+
+
+    @paid = orders(:two)
+    @paid.credit_card_exp_month = Date.today.month
+    @paid.credit_card_exp_year = Date.today.year
+
+    @shipped = orders(:three)
+    @shipped.credit_card_exp_month = Date.today.month
+    @shipped.credit_card_exp_year = Date.today.year
+  end
+
+  describe 'relations' do
+    it "has a list of order_items" do
+      expect(@pending).must_respond_to :order_items
+      @pending.order_items.each do |order_item|
+        expect(order_item).must_be_kind_of OrderItem
+      end
+    end
+  end
 
   it "must be valid" do
-    value(order).must_be :valid?
+
   end
 end
