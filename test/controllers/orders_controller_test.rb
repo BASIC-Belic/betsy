c130bad011f1237dce55ddeeb9201721587a444c
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-28 11:19:59 -0700

    befor eblock to order

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 describe OrdersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  before do
+    @pending = orders(:one)
+    @pending.credit_card_exp_month = Date.today.month
+    @pending.credit_card_exp_year = Date.today.year % 1000
+
+    @empty = Order.new
+
+    @paid = orders(:two)
+    @paid.credit_card_exp_month = Date.today.month
+    @paid.credit_card_exp_year = Date.today.year % 1000
+
+    @shipped = orders(:three)
+    @shipped.credit_card_exp_month = Date.today.month
+    @shipped.credit_card_exp_year = Date.today.year % 1000
+  end
+
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -7,14 +7,6 @@ describe Order do
     @pending.credit_card_exp_year = Date.today.year % 1000
 
     @empty = Order.new
-
-    @paid = orders(:two)
-    @paid.credit_card_exp_month = Date.today.month
-    @paid.credit_card_exp_year = Date.today.year % 1000
-
-    @shipped = orders(:three)
-    @shipped.credit_card_exp_month = Date.today.month
-    @shipped.credit_card_exp_year = Date.today.year % 1000
   end
 
   describe 'relations' do
